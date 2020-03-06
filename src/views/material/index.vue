1d250fdc345322677a5a8a8b23f1305c096b00af
xm107 <1428386916@qq.com>
2020-03-06 23:53:02 +0800

    素材列表-分页组件及请求-加载状态

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,6 +1,6 @@
 <template>
 <!-- el-card布局 -->
-    <el-card>
+    <el-card v-loading="loading">
         <!-- 面包屑 -->
         <bread-crumb slot="header">
         <!-- 具名插槽 template -->
@@ -8,6 +8,7 @@
             素材管理
         </template>
         </bread-crumb>
+
         <!-- 放置一个上传组件 el-row可以布局-->
         <el-row type="flex" justify="end">
             <!-- 上传组件要求必须传action属性 不传就会报错 可以给空字符串 show-file-list 是否显示已上传文件列表 -->
@@ -46,6 +47,25 @@
                 </div>
             </el-tab-pane>
         </el-tabs>
+
+        <!-- 放置一个公共分页 -->
+        <el-row type="flex" justify="center" style="height:80px" align="middle">
+          <!-- 放置分页组件
+              total:总条数
+              current-pageg 当前页码
+              page-size  每页多少条
+              监听 分页组件的切换事件
+           -->
+           <el-pagination background
+           :total="page.total"
+           :current-page="page.currentPage"
+           :page-size="page.pageSize"
+           layout="prev,pager,next"
+           @current-change="changePage"
+           >
+           </el-pagination>
+
+        </el-row>
     </el-card>
 </template>
 
@@ -54,7 +74,14 @@ export default {
   data () {
     return {
       activeName: 'all', // 当前激活收藏   默认选中全部收藏
-      list: []// 全部素材的数据
+      list: [], // 全部素材的数据
+      loading: false, // 控制loading遮罩层的显示或者隐藏
+      // 专门存放分页信息的对象
+      page: {
+        currentPage: 1, // 默认第一页
+        total: 0, // 当前总数
+        pageSize: 4// 每页条数
+      }
     }
   },
   methods: {
@@ -77,22 +104,40 @@ export default {
         this.$message.error('上传素材失败')
       })
     },
+
+    // 该方法在分页切换时执行
+    changePage (newPage) {
+      // 传入一个新页
+      this.page.currentPage = newPage// 将心页码赋值给当前页码数据
+      this.getMaterial()// 重新拉取数据
+    },
+
     getMaterial () {
+      this.loading = true// 打开遮罩层
       this.$axios({
         url: '/user/images', // 请求地址
         params: {
-          collect: this.activeName === 'collect'// 根据当前的页签变活
+          collect: this.activeName === 'collect', // 根据当前的页签变活
+          page: this.page.currentPage, // 取页码变量中的值 因为只要页码变量一变 获取的数据跟着变
+          per_page: this.page.pageSize// 获取每页数量
         }, // get参数 jquery参数
         data: {}// data参数放的是body参数
       }).then(result => {
         // 将返回数据 赋值到data中
         this.list = result.data.results
+        // 总条数赋值给total变量
+        this.page.total = result.data.total_count// 总数  全部素材的总数  收藏素材的总数 总数 跟随 当前页签变化而变化
+        this.loading = false// 数据请求完毕，关闭遮罩层
       })
     },
+
     // 切换页签事件
     changeTab () {
-    // 在切换事件中
-    // 可以根据当前 active='collect' 获取收藏素材
+      this.page.currentPage = 1// 将页码重置为第一页 以为页码变了 数据变了
+      // 在切换事件中
+      // 可以根据当前 active='collect' 获取收藏素材
+      // 可以根据当前 activeName来决定是获取哪个方面 的数据
+      // activeName === 'all' 获取所有的素材  activeName = 'collect' 获取收藏素材
       this.getMaterial()// 直接调用获取素材的方法
     }
   },
