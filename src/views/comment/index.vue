14932e6bac5c587d5b9a9598a905360ca2d96db9
xm107 <1428386916@qq.com>
2020-03-06 22:16:42 +0800

    大数字处理成功-打开和关闭评论功能成功

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -25,9 +25,24 @@
           <!-- 文本内容要根据当前行里面的评论状态决定显示还是隐藏 -->
           <el-button @click="openOrClose(obj.row)" type="text"  size="small">{{obj.row.comment_status?'关闭评论':'打开评论'}}</el-button>
         </template>
-
       </el-table-column>
     </el-table>
+    <!-- 放置分页组件 -->
+    <el-row style="height:80px" type="flex" align="middle" justify="center">
+      <!--
+        分页组件需要 动态的数据
+        total 当前的总数
+        current-page 当前的页码
+        page-size 每条多少页
+        监听 current-change 事件
+       -->
+       <el-pagination background layout="prev,pager,next"
+         :current-page="page.currentPage"
+         :total='page.total'
+         :page-size="page.pageSize"
+         @current-change='changePage'
+         ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -35,18 +50,35 @@
 export default {
   data () {
     return {
+      // 分页参数单独放置一个对象 让数据更清晰
+      page: {
+        total: 0, // 默认总数是0
+        currentPage: 1, // 默认的页码 是第一个页  决定了当前页码是第几页
+        pageSize: 10// page-size的默认值是10
+      },
       list: [
 
       ]
     }
   },
   methods: {
+    // 页码改变事件 newPage 就是点击切换的最新页码
+    changePage (newPage) {
+      // newPage是最新的切换页码
+      // 将最新的页码设置给当前的页码
+      this.page.currentPage = newPage
+      // 重新获取数据
+      this.getComment()
+    },
     // 获取评论数据
     getComment () {
       this.$axios({
         url: '/articles', // 请求地址
+        // 接口 如果不传分页的数据 默认查第一页的数据
         params: {
-          response_type: 'comment'// 此参数用来控制获取数据类型
+          response_type: 'comment', // 此参数用来控制获取数据类型
+          page: this.page.currentPage, // 查对应页码
+          per_page: this.page.pageSize// 查十条
         }
         // query参数应该在哪传axios
         // params 传get参数也就是query参数
@@ -54,6 +86,8 @@ export default {
       }).then(result => {
         // 将返回中的数组给list
         this.list = result.data.results
+        // 在获取完数据后 将 总数赋值给total
+        this.page.total = result.data.total_count// 将总数赋值
       })
     },
     formatterBool (row, column, cellValue, index) {
@@ -71,11 +105,15 @@ export default {
       this.$confirm(`是否确认${mess}评论`, '提示').then(() => {
         // 调用打开或者关闭接口
         this.$axios({
-          url: '/comment/status', // 请求地址
+          url: '/comments/status', // 请求地址
           method: 'put', // 请求类型
           // query
           params: {
-            article_id: row.id// 要求参数的文章
+            // 为什么评论会失败 就是因为 原来 给你传了 9152 你回传了 9200
+            // 所以我们用大数字包 保证 9152不被转化 就可以使用原来的功能
+            article_id: row.id.toString()// 要求参数的文章id 将BigNumber类型转化为字符串
+          // 前端传字符串到后端 只要和原数字一致  后端会自动将字符串转成大数字
+            // 只需要保证 id 和传过来的id一致就行
           },
           data: {
             // body参数
@@ -92,7 +130,7 @@ export default {
     }
   },
   created () {
-  // 在钩子函数中 直接获取数据
+  // 在钩子函数中 直接获取数据  获取第一页数据
     this.getComment()
   }
 }
